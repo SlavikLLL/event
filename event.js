479f9659d6f2b94ad85504bc36a744a674615077
SlavikLLL <listopads091@gmail.com>
2021-06-21 02:06:17 +0300

    h

--- event.js
+++ event.js
@@ -1,37 +1,14 @@
-let enter = document.getElementById('enter');
-
-
-function clearText(){
-    
-    text.value = "";
+let enter = document.getElementById("enter");
+let text = document.getElementById("text");
+function clearText() {
+  text.value = "";
 }
-function eventText(){
-    let  text = document.getElementById('text');
-    let value = text.value;
-    let ul = document.createElement('ul');
-    document.body.append(ul);
-   if(value == ""){
-       alert("Ошибка");
-   }
-   else{
-     let li = document.createElement('li');
-     li.innerHTML = value;
-     clearText();
-     ul.appendChild(li);
-     
-    
+function eventText() {
+  let value = text.value;
+  let li = document.createElement("li");
+  li.innerHTML = value;
+  clearText();
 
-     
-
-     
-      
-   }
-  
+  list.prepend(li);
 }
-
-
-//
-
 enter.onclick = eventText;
-
-
